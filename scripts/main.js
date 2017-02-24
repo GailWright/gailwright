aa56c75f4990d8e583d85b1eb3304a9438805638
GailWright <hello@gailwrightva.com.au>
2017-02-24 22:55:05 +0800

    Added form, rates , etc

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
-$(document).ready(function()){
+$(document).ready(function() {
   //Query code here
+  $('.interactive-block').click(function(){
+
+    $(this).find('.interactive-content').toggleClass('active');
+
+  });
 });
