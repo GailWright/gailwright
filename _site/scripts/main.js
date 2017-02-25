e9c3aff75f5740424f8a44b79692869f87abd6f5
GailWright <hello@gailwrightva.com.au>
2017-02-25 18:02:21 +0800

    Changes to Services page
    Didnt like the Jekyll component on the Services page - items still
    remain at the moment ( to be removed)

--- _site/scripts/main.js
+++ _site/scripts/main.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
   //Query code here
+
+
+
+
   $('.interactive-block').click(function(){
-
     $(this).find('.interactive-content').toggleClass('active');
-
   });
+
+
+  $('.nav-button').click(function() {
+    $('nav').toggleClass('mobile-nav-active');
+  });
+
+
 });
